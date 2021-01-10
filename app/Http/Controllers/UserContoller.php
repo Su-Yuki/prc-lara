a9f4c9ac3a7b301d0e84042afe25d27770fb0f30
Su-Yuki <76637981 +Su-Yuki@users.noreply.github.com>
2021-01-10 23:58:55 +0900

    no message

--- app/Http/Controllers/UserContoller.php
+++ app/Http/Controllers/UserContoller.php
@@ -16,3 +16,6 @@ class UserContoller extends Controller
 
     }
 }
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/users', 'UserContoller@index');
 Route::get('/bookmarks', 'BookmarkContoller@index');
 
 
+
+
